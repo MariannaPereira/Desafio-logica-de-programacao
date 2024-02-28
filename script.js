7bc69b870a880e0c6f6dc7551257946a764666bc
MariannaPereira <maricerqueira6567@gmail.com>
2024-02-28 19:03:24 -0400

    desafio de logica

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log ("Ano Novo está chegando");
+
+var cantora = "Mariah Carey"
+var idade = 55
+var anoAtual = 2024
+const dataDeNascimento = anoAtual - idade
+console.log (dataDeNascimento)
+
+let quartaFeira = true 
+console.log(typeof quartaFeira) 
+
+if (idade>=18){
+console.log("Você é maior de idade")
+
+}
+else{
+console.log("Você é menor de idade")
+}
+
+let mes ="Agosto"
+
+if(mes==="Agosto" || mes==="Dezembro" || mes==="Junho"){
+console.log("Uma das opções está correta")
+}
+else{
+console.log ("O mês escolhido está errado")
+}
